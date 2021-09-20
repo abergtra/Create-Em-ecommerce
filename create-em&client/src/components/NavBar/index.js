03fbca117649280376e6571602da60bd0bc226b9
Asher <asher.bergtraun@gmail.com>
2021-09-20 07:01:23 -0700

    added return fro Navbar to present logo

--- create-em&client/src/components/NavBar/index.js
+++ create-em&client/src/components/NavBar/index.js
@@ -6,7 +6,18 @@ import { Link } from "react-router-dom";
 //NavBar component function
 function NavBar() {
     //HTML for header logo and call function show conditional options
-    return {
-        <header
-    }
+    return (
+        <header className="flex-row px-1">
+            <h1>
+                {/* Insert  */}
+                <Link to="/">
+                    <img src="/images/logo.png" alt="logo"/>
+                </Link>
+            </h1>
+            <nav>
+                {showNavigation()}
+            </nav>
+        </header>
+    )
+        
 }
