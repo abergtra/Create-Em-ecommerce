e3962396686b63c08fffb3f119dbc18141b388e0
Asher <asher.bergtraun@gmail.com>
2021-09-20 06:35:53 -0700

    import react and authorization for navbar component

---
+++ create-em&client/src/components/NavBar/index.js
@@ -0,0 +1,12 @@
+//import react and authorization
+import React from "react";
+import Auth from "../../utils/auth";
+import { Link } from "react-router-dom";
+
+//NavBar component function
+function NavBar() {
+    //HTML for header logo and call function show conditional options
+    return {
+        <header
+    }
+}
