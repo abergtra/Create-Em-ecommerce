08d561a12c3be338de4c7faad8080c568f75c645
Asher <asher.bergtraun@gmail.com>
2021-09-20 06:27:26 -0700

    import react products and categories in Home.js

--- create-em&client/src/pages/Home.js
+++ create-em&client/src/pages/Home.js
@@ -1,1 +1,6 @@
+//import react
 import React, { useState } from "react";
+
+//import products and categories
+import ProductList from "../components/ProductList";
+import CategoryMenu from "../components/CategoryMenu";
