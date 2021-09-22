757736b4d270e4c5bea9b19f627ec5ba45c043f0
Asher <asher.bergtraun@gmail.com>
2021-09-22 13:43:20 -0700

    import for login page and build function structure

---
+++ client/src/pages/Login.js
@@ -0,0 +1,12 @@
+//import react and apollo
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import { LOGIN } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+function Login(props) {
+    const;
+}
+
+export default Login;
