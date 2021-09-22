c7dff0a5b9fe875ad7b712dac189e7858ba63727
Asher <asher.bergtraun@gmail.com>
2021-09-22 13:01:34 -0700

    completed details page

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -8,7 +8,46 @@ import spinner from '../assets/spinner.gif';
 
 //Function to display detailed information about each product
 function Detail() {
+    const { id } = useParams();
 
+    const [currentProduct, setCurrentProduct] = useState({});
+
+    const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+    const products = data?.products || [];
+
+    useEffect(() => {
+        if (products.length) {
+            setCurrentProduct(products.find((product) => product._id === id));
+        }
+    }, [products, id]);
+
+    //return HTML to display product details
+    return (
+        <>
+          {currentProduct ? (
+            <div className="container my-1">
+              <Link to="/">← Back to Products</Link>
+    
+              <h2>{currentProduct.name}</h2>
+    
+              <p>{currentProduct.description}</p>
+    
+              <p>
+                <strong>Price:</strong>${currentProduct.price}{' '}
+                <button>Add to Cart</button>
+                <button>Remove from Cart</button>
+              </p>
+    
+              <img
+                src={`/images/${currentProduct.image}`}
+                alt={currentProduct.name}
+              />
+            </div>
+          ) : null}
+          {loading ? <img src={spinner} alt="loading" /> : null}
+        </>
+      );
 }
 
 export default Detail;
