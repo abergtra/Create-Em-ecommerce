5f822904a067243d5046977fb11143e67fd2fffa
Asher <asher.bergtraun@gmail.com>
2021-09-22 12:42:44 -0700

    added import for detail page

---
+++ client/src/pages/Detail.js
@@ -0,0 +1,7 @@
+//import react & apollo
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+
+import { QUERY_PRODUCTS } from '../utils/queries';
+import spinner from '../assets/spinner.gif';
