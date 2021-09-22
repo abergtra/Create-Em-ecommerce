c88ab126adf9a3120710faf715d53a8b9ca337f5
Asher <asher.bergtraun@gmail.com>
2021-09-22 13:45:52 -0700

    added functions to handle login submit and state change

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -6,7 +6,29 @@ import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 function Login(props) {
-    const;
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [login, { error }] = useMutation(LOGIN);
+
+    //function to respond to login submit
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const mutationResponse = await login({
+                variables: { email: formState.email, password: formState.password },
+            });
+            const token = mutationResponse.data.login.token;
+            Auth.login(token);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+          ...formState,
+          [name]: value,
+        });
+    };
 }
 
 export default Login;
