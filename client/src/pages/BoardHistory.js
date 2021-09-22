181dc872bc89f743a340e30332bfb6b4dd452df8
Asher <asher.bergtraun@gmail.com>
2021-09-22 12:30:03 -0700

    added imports for board history page

---
+++ client/src/pages/BoardHistory.js
@@ -0,0 +1,6 @@
+//import react
+import React from 'react';
+import { Link } from 'react-router-dom';
+//import query
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../utils/queries';
