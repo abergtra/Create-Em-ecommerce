cfd2fdf9485c33dfa1650390f2faefc2f9b8eb8e
Asher <asher.bergtraun@gmail.com>
2021-09-22 14:03:37 -0700

    comment out page elements needed

---
+++ client/src/pages/Signup.js
@@ -0,0 +1,17 @@
+//import react and auth and apollo
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import Auth from '../utils/auth';
+import { ADD_USER } from '../utils/mutations';
+
+function Signup(props) {
+    const;
+    //function to respond to signup submit
+
+    //function for state change
+
+    //display HTML for UI signup
+}
+
+export default Signup;
