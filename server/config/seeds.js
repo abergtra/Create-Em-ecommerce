03302b818a668767ff294d8fa9be0ea6f8dd9d04
Newton <narmstrongv@gmail.com>
2021-09-22 18:41:02 -0700

    added seeds

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -35,22 +35,114 @@ db.once('open', async () => {
       
     },
     {
-        name: 'Vegetarian Large Board',
-        description:
-          'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-        image: 'vegetarian-large.jpg',
-        category: categories[2]._id,
-        price: 13.99,
+      name: 'Vegetarian Large Board',
+      description:
+        'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
+      image: 'vegetarian-large.jpg',
+      category: categories[2]._id,
+      price: 13.99,
     
     },
     {
-            name: 'Small Classic Board',
-            description: 'Classic board with meat, cheese, and various fruit.',
-            image: 'classic-small.jpg',
-            category: categories[0]._id,
-            price: 15.00,
+      name: 'Small Classic Board',
+      description:
+       'Classic board with meat, cheese, and various fruit.',
+      image: 'classic-small.jpg',
+      category: categories[0]._id,
+      price: 15.00,
     
     },
+    {
+      name: 'Kosher Large Board 2',
+      description:
+       'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+      image: 'kosher-large2.jpg',
+      category: categories[1]._id,
+      price: 15.00,
+    
+    },
+    {
+      name: 'Classic Large Board 2',
+      description:
+       'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+      image: 'classic-large2.jpg',
+      category: categories[0]._id,
+      price: 15.00,
+    
+    },
+    {
+      name: 'Kosher Small Board',
+      description:
+       'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+      image: 'kosher-small.jpg',
+      category: categories[1]._id,
+      price: 15.00,
+    
+    },
+    {
+      name: 'Classic Large Board 3',
+      description:
+       'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+      image: 'classic-large3.jpg',
+      category: categories[0]._id,
+      price: 15.00,
+    
+    },
+    {
+      name: 'Vegetarian Box',
+      description:
+       'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+      image: 'vegetarian-box.jpg',
+      category: categories[2]._id,
+      price: 15.00,
+    
+    },
+    {
+      name: 'Kosher Box',
+      description:
+       'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+      image: 'kosher-box.jpg',
+      category: categories[1]._id,
+      price: 15.00,
+    
+    },
+    {
+      name: 'Classic Box 1',
+      description:
+       'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+      image: 'classic-box1.jpg',
+      category: categories[0]._id,
+      price: 15.00,
+    
+    },
+    {
+      name: 'Classic Box 2',
+      description:
+       'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+      image: 'classic-box2.jpg',
+      category: categories[0]._id,
+      price: 15.00,
+    
+    },
+    {
+      name: 'Classic Cup 1',
+      description:
+       'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+      image: 'classic-cup1.jpg',
+      category: categories[0]._id,
+      price: 15.00,
+    
+    },
+    {
+      name: 'Classic Cup 2',
+      description:
+       'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+      image: 'classic-cup2.jpg',
+      category: categories[0]._id,
+      price: 15.00,
+    
+    },
+
    
   ]);
 
